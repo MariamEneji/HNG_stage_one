5ab2e60cfab4cc85c866fadc78ae037c800bcb27
Mariam Eneji <enejimariam1997@gmail.com>
2023-09-10 18:16:28 +0100

    milliseconds commit

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const showTime = () => {
         'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
     ]
 
-    let hours = date.getHours()
-    let minutes = date.getMinutes()
-    let seconds = date.getSeconds()
+    let hours = date.getHours() * 3600000 
+    let minutes = date.getMinutes() * 60000
+    let seconds = date.getSeconds() * 1000
     let milliseconds = date.getMilliseconds()
 
     let weekDay = weekday[date.getDay()]
 
-    let time = (hours - 1) + ':' + minutes + ':' + seconds + ':' + milliseconds 
+    let time = Date.now()
 
 
 
